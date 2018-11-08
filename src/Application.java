91aca0a32e619ef6f670d37fc6851bb52771ea92
valentin <vboudeau.info@gmail.com>
2018-11-08 11:30:22 +0100

    Fin

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,17 @@
+import java.util.Timer;
 
 public class Application {
-	public static void main(String[] args) {
-			
-	}
+    public static void main(String[] args) {
+        Ensemble e = new Ensemble(4);
+
+        StopWatch timer = new StopWatch().start();
+
+        for (int i = 0; i < 100; i++) {
+            e.insert(i);
+        }
+
+
+
+
+    }
 }
