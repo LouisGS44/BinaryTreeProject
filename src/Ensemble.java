59d16127e40314c0dd2fe301e93dab1b498da536
jbourdoi <jbourdoi@LFR024988.corp.capgemini.com>
2018-11-08 09:56:42 +0100

    Boujour

--- src/Ensemble.java
+++ src/Ensemble.java
@@ -1,14 +1,23 @@
 
 public class Ensemble {
+    private int valeur;
+    private Ensemble gauche, droit;   
+
+   // CONSTRUCTEURS
+    public Ensemble(int x) {
+        valeur = x;
+    }
+
+    public Ensemble(int x, Ensemble g, Ensemble d) {
+        valeur = x;
+        gauche = g;
+        droit = d;
+    } 
 	
-	public void insert(int x) {
-		
+	public void insert(int x) {	
 	}
-	
-	public void delete(int x) {
-		
+	public void delete(int x) {	
 	}
-	
 	public boolean contains(int x) {
 		return false;
 	}
